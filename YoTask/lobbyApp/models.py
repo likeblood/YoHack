d5d6b74e11551b2356016d07cfdc2e1af9a12e28
Anatoly <krest.66@mail.ru>
2020-04-24 13:13:17 +0300

    added basic models
    lobby, room, task

--- YoTask/lobbyApp/models.py
+++ YoTask/lobbyApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# waiting for User model to import it
+
+
+class Lobby(models.Model):
+	creater = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	lobby_name = models.CharField(max_length = 30)
+	lobby_description = models.TextField()
