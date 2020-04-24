5f836530d526b399a7983ace9ded15e6c97dcfc4
Anatoly <krest.66@mail.ru>
2020-04-24 14:33:50 +0300

    changed urls.py

--- YoTask/roomApp/urls.py
+++ YoTask/roomApp/urls.py
@@ -3,12 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.tasks, name='tasks'),
-    url(r'^$', views.tasksActive, name='tasksActive'),
+    url(r'^$', views.tasksActive, name='room'),
     url(r'^$', views.tasksClosed, name='tasksClosed'),
     url(r'^$', views.tasksAll, name='tasksAll'),
 
-    
+
     url(r'^$', views.toDoList, name='toDoList'),
     url(r'^$', views.Done, name='Done'),
 ]
--- YoTask/roomApp/views.py
+++ YoTask/roomApp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+''' tasks page '''
+
+def tasks(request):
+	return render(request, 'roomApp/room.html')
+
+def tasks(request):
+	return render(request, 'roomApp/tasksClosed.html')
+
+def tasks(request):
+	return render(request, 'roomApp/tasksAll.html')
+
+
+
+
+
+''' To-Do user's page '''
+
+def tasks(request):
+	return render(request, 'roomApp/toDoList.html')
+
+
+def tasks(request):
+	return render(request, 'roomApp/Done.html')
