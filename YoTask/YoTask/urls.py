6df3a8be7cf0e546db4429fd69d0e26a541989f4
Anatoly <krest.66@mail.ru>
2020-04-25 21:17:44 +0300

    Update urls.py

--- YoTask/YoTask/urls.py
+++ YoTask/YoTask/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     url(r'^lobby/$', views.join_lobby, name='join_lobby'),  # page with lobby login and lobby creating
-    # url(r'^lobby/(\d+)/$', views.lobby, name='lobby'),              # lobby
+    url(r'^lobby/(\d+)/$', views.lobby, name='lobby'),              # lobby
 
     # url(r'^room/(\d+)/issues/$', views.issues, name='issues'),                   # room issues
     # url(r'^room/(\d+)/issues/(\d+)/$', views.about_issue, name='about_issue'),   # about issue
